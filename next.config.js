77f1a8bdcfc20f5dd3dec8a8642dcba94849dc48
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2022-10-01 17:19:04 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ['content.linkedin.com','rb.gy'],
+  },
 }
 
 module.exports = nextConfig
